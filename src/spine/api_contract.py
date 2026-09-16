9d3be797f70303c627b8b94b7e6f0fc70de5925d
Nate0-1999 <142685530+Nate0-1999@users.noreply.github.com>
2026-09-15 19:17:16 -0500

    M3CM: version the authenticated compaction notification contract

--- src/spine/api_contract.py
+++ src/spine/api_contract.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 from hashlib import sha256
 from typing import Any
 
-API_CONTRACT_VERSION = "0.1.16"
+API_CONTRACT_VERSION = "0.1.17"
 
 
 class ApiContractDriftError(RuntimeError):
